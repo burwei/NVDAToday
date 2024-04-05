015e4c8708d293f493596ad3a5dd507a70afe901
burwei <thisisburwei@gmail.com>
2024-04-05 02:00:13 +0200

    add processBets() and an unimplmented getNvdaPrice()

--- smart_contract/src/NvdaToday.sol
+++ smart_contract/src/NvdaToday.sol
@@ -1,18 +1,25 @@
-// SPDX-License-Identifier: UNLICENSED
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.24;
 
+import "BokkyPooBahsDateTimeLibrary/contracts/BokkyPooBahsDateTimeLibrary.sol";
+import {console} from "forge-std/Test.sol";
+
 contract NvdaToday {
-    uint256 public lastPrice;
-    uint256 public totalStakeBetHigher;
-    uint256 public totalStakeBetLower;
+    uint public lastPrice;
+    uint public totalStakeBetHigher;
+    uint public totalStakeBetLower;
     address[] private playersBetHigher;
     address[] private playersBetLower;
-    mapping(address => uint256) private stakesBetHigher;
-    mapping(address => uint256) private stakesBetLower;
+    mapping(address => uint) private stakesBetHigher;
+    mapping(address => uint) private stakesBetLower;
+
+    // Make the contract payable
+    receive() external payable {}
+    fallback() external payable {}
 
     function betHigher() public payable {
         require(msg.value > 0, "You must bet more than 0");
-        uint256 stack = stakesBetHigher[msg.sender];
+        uint stack = stakesBetHigher[msg.sender];
         if (stack == 0) {
             playersBetHigher.push(msg.sender);
         }
@@ -23,7 +30,7 @@ contract NvdaToday {
 
     function betLower() public payable {
         require(msg.value > 0, "You must bet more than 0");
-        uint256 stack = stakesBetLower[msg.sender];
+        uint stack = stakesBetLower[msg.sender];
         if (stack == 0) {
             playersBetLower.push(msg.sender);
         }
@@ -32,15 +39,15 @@ contract NvdaToday {
         stakesBetLower[msg.sender] = stack;
     }
 
-    function settleBets(uint256 price) internal {
-        uint256 totalWinning = totalStakeBetHigher + totalStakeBetLower;
+    function settleBets(uint price) internal {
+        uint totalWinning = totalStakeBetHigher + totalStakeBetLower;
         require(
             address(this).balance >= totalWinning,
             "Contract does not have enough balance to cover winnings"
         );
 
         if (price > lastPrice && playersBetHigher.length > 0) {
-            for (uint256 i = 0; i < playersBetHigher.length; i++) {
+            for (uint i = 0; i < playersBetHigher.length; i++) {
                 payable(playersBetHigher[i]).transfer(
                     totalWinning * 1000 / totalStakeBetHigher * stakesBetHigher[playersBetHigher[i]] / 1000
                 );
@@ -48,7 +55,7 @@ contract NvdaToday {
             totalStakeBetHigher = 0;
             totalStakeBetLower = 0;
         } else if (price < lastPrice && playersBetLower.length > 0) {
-            for (uint256 i = 0; i < playersBetLower.length; i++) {
+            for (uint i = 0; i < playersBetLower.length; i++) {
                 payable(playersBetLower[i]).transfer(
                     totalWinning * 1000 / totalStakeBetLower * stakesBetLower[playersBetLower[i]] / 1000
                 );
@@ -61,23 +68,30 @@ contract NvdaToday {
 
         delete playersBetHigher;
         delete playersBetLower;
-        for (uint256 i = 0; i < playersBetHigher.length; i++) {
+        for (uint i = 0; i < playersBetHigher.length; i++) {
             delete stakesBetHigher[playersBetHigher[i]];
         }
-        for (uint256 i = 0; i < playersBetLower.length; i++) {
+        for (uint i = 0; i < playersBetLower.length; i++) {
             delete stakesBetLower[playersBetLower[i]];
         }
 
         lastPrice = price;
     }
 
-    function getBalance() public view returns(uint256) {
-        return address(this).balance;
+    // TODO: implement this function
+    function getNvdaPrice() public view returns(uint) {
+        return lastPrice;
     }
 
-    // Function to receive Ether. msg.data must be empty
-    receive() external payable {}
+    function processBets() public {
+        uint currentHour = BokkyPooBahsDateTimeLibrary.getHour(block.timestamp);
+        require(currentHour >= 20 && currentHour <= 23, "You can only call this function between 20:00 and 23:00 UTC");
 
-    // Fallback function is called when msg.data is not empty
-    fallback() external payable {}
+        uint price = getNvdaPrice();
+        settleBets(price);
+    }
+
+    function getBalance() public view returns(uint) {
+        return address(this).balance;
+    }
 }
--- smart_contract/test/NvdaToday.t.sol
+++ smart_contract/test/NvdaToday.t.sol
@@ -1,11 +1,11 @@
-// SPDX-License-Identifier: UNLICENSED
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.24;
 
 import {Test, console} from "forge-std/Test.sol";
 import {NvdaToday} from "../src/NvdaToday.sol";
 
 contract PublicNvdaToday is NvdaToday {
-    function publicSettleBets(uint256 price) public {
+    function publicSettleBets(uint price) public {
         settleBets(price);
     }
 }
@@ -13,13 +13,19 @@ contract PublicNvdaToday is NvdaToday {
 contract NvdaTodayTest is Test {
     PublicNvdaToday nvdaToday;
 
+    // Function to receive Ether. msg.data must be empty
+    receive() external payable {}
+
+    // Fallback function is called when msg.data is not empty
+    fallback() external payable {}
+
     function setUp() public {
         nvdaToday = new PublicNvdaToday();
     }
 
     function testBetHigher() public {
-        uint256 initCallerBalance = address(this).balance;
-        uint256 initCalleeBalance = address(nvdaToday).balance;
+        uint initCallerBalance = address(this).balance;
+        uint initCalleeBalance = address(nvdaToday).balance;
 
         // First test case: bet 1 ether
         nvdaToday.betHigher{value: 1 ether}();
@@ -35,8 +41,8 @@ contract NvdaTodayTest is Test {
     }
 
     function testBetLower() public {
-        uint256 initCallerBalance = address(this).balance;
-        uint256 initCalleeBalance = address(nvdaToday).balance;
+        uint initCallerBalance = address(this).balance;
+        uint initCalleeBalance = address(nvdaToday).balance;
 
         // First test case: bet 1 ether
         nvdaToday.betLower{value: 1 ether}();
@@ -52,8 +58,8 @@ contract NvdaTodayTest is Test {
     }
 
     function testSettleBets() public {
-        uint256 initCallerBalance = address(this).balance;
-        uint256 initCalleeBalance = address(nvdaToday).balance;
+        uint initCallerBalance = address(this).balance;
+        uint initCalleeBalance = address(nvdaToday).balance;
 
         // First test case: price is higher than last price, bet higher
         nvdaToday.betHigher{value: 1 ether}();
@@ -72,9 +78,23 @@ contract NvdaTodayTest is Test {
         assertEq(address(nvdaToday).balance,initCalleeBalance + 1 ether);
     }
 
-    // Function to receive Ether. msg.data must be empty
-    receive() external payable {}
+    function testProcessBets() public {
+        // Set block.timestamp to a time within 20:00 - 23:00 UTC
+        uint validTimestamp = 1712696400; // This is 2024-04-09 @ 21:00 (UTC)
+        vm.warp(validTimestamp);
 
-    // Fallback function is called when msg.data is not empty
-    fallback() external payable {}
+        // This function's success depends on other contract state (like settleBets and getNvdaPrice).
+        // For this example, we'll just call the function assuming those conditions are handled.
+        nvdaToday.processBets();
+    }
+
+    function testProcessBetsOutsideAllowedHours() public {
+        // Set block.timestamp to a time outside 20:00 - 23:00 UTC
+        uint invalidTimestamp = 1712667600; // This is 2024-04-09 @ 13:00 (UTC)
+        vm.warp(invalidTimestamp); // 'vm.warp' manipulates the block timestamp
+
+        // Attempt to call processBets and expect it to fail
+        vm.expectRevert(bytes("You can only call this function between 20:00 and 23:00 UTC"));
+        nvdaToday.processBets();
+    }
 }
